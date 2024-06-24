f9c83b0743dcde7fe8dcb74163cf30d5ac766d4b
Jim Ries <RiesJ@missouri.edu>
2024-06-24 15:30:23 -0500

    Initial roll-in

---
+++ lab8.5.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lab8.5.h"
+
+double mypow(int base, int power)
+{
+    // You need to implement this
+    return 0.0;
+}
+
+
+void PrintBaseBallPlayers(PBaseBallPlayer players, int length)
+{
+    // You should probably call PrintBaseBallPlayer() in here to print
+    // a single player.
+}
+
+void SortBaseBallPlayers(PBaseBallPlayer players, int length)
+{
+    // You might want to use SwapPlayer() to swap a player
+    // from one spot in the array with a player in another
+    // spot in the array.
+
+
+    // You also might need another function to figure out which
+    // player has the highest average in an array.  You can use 
+    // the AVG() function to get the average for a particular
+    // player.
+}
